30f3d8b4970c4d9905cfcc8629a034c008cf54cb
Hongbao Chen <microcore@yahoo.com.cn>
2012-02-24 17:59:56 +0800

    Add methods to initialize the ControlPanel. 1) Adjust it location when appearing. 2) Encapsulate the operations into methods.

--- JavaSoundRun/src/chb/gui/ControlPanel.java
+++ JavaSoundRun/src/chb/gui/ControlPanel.java
@@ -3,30 +3,80 @@ package chb.gui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 public class ControlPanel extends JFrame {
 
     // The size of the main frame window.
-    public int WIN_HEIGHT = 400;
-    public int WIN_WIDTH = 500;
+    public int WIN_HEIGHT = 0;
+    public int WIN_WIDTH = 0;
+
+    // Position of the window when it first appears.
+    public int WIN_X = 0;
+    public int WIN_Y = 0;
 
     // The default title of the main frame window.
-    public String WIN_TITLE = "语音合成系统v1-陈宏葆";
+    public String WIN_TITLE = "";
 
 
     public ControlPanel() throws HeadlessException {
+        super();
 
-        this.setSize(this.WIN_WIDTH, this.WIN_HEIGHT);
-        this.setTitle(this.WIN_TITLE);
+        setCPSize();
+        setCPTitle();
+        setCPLocation();
         this.addWindowListener(new CPWindowListener().setParentFrame(this));
 
+        setCPParameters();
         this.setVisible(true);
     }
 
+    /**
+     * Static method to start the application.
+     *
+     * @return
+     */
     public static int work() {
         new ControlPanel();
         return 0;
+    }
+
+    /**
+     * Set the location of main window when it first appears.
+     */
+    private void setCPLocation() {
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        if (d == null) {
+            return;
+        }
+
+        this.WIN_X = (d.width - this.WIN_WIDTH) / 2;
+        this.WIN_Y = (d.height - this.WIN_HEIGHT) / 2;
+    }
+
+    /**
+     * Set the title of the main window.
+     */
+    private void setCPTitle() {
+        this.WIN_TITLE = "语音合成系统v1-陈宏葆";
+    }
+
+    /**
+     * Set the size of main window.
+     */
+    private void setCPSize() {
+        this.WIN_WIDTH = 700;
+        this.WIN_HEIGHT = 400;
+    }
+
+    /**
+     * Set all the parameters into the super class
+     * to control the JFrame behaviour.
+     */
+    private void setCPParameters() {
+        super.setLocation(this.WIN_X, this.WIN_Y);
+        super.setTitle(this.WIN_TITLE);
+
+        super.setSize(this.WIN_WIDTH, this.WIN_HEIGHT);
+        super.setResizable(false);
     }
 }
